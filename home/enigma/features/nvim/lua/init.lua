414c3609ec4c03cc998357197d7118eb05255a92
oliverwiegers <oliver.wiegers@gmail.com>
2023-11-16 07:17:24 +0100

    Change bat theme for fzf file preview in neovim

--- home/enigma/features/nvim/lua/init.lua
+++ home/enigma/features/nvim/lua/init.lua
@@ -1,9 +1,12 @@
--- Set colorscheme
+-- Set colorscheme.
 vim.cmd "colorscheme gruvbox"
 vim.g.gruvbox_contrast_dark = "hard"
 
--- Set leader key to space
+-- Set leader key to space.
 vim.g.mapleader = " "
 
--- Disable mouse
+-- Disable mouse.
 vim.opt.mouse = "" 
+
+-- Set theme for fzf preview using bat.
+vim.fn.setenv("BAT_THEME", "gruvbox-dark")
