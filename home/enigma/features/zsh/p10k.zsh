f4840df723dcdcd61a6eab6d5590e142f0f3ef8e
oliverwiegers <oliver.wiegers@gmail.com>
2023-12-15 10:17:34 +0100

    Update p10k direnv config

--- home/enigma/features/zsh/p10k.zsh
+++ home/enigma/features/zsh/p10k.zsh
@@ -555,7 +555,8 @@
   # Direnv color.
   typeset -g POWERLEVEL9K_DIRENV_FOREGROUND=3
   # Custom icon.
-  # typeset -g POWERLEVEL9K_DIRENV_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_DIRENV_SUFFIX=' direnv'
+  typeset -g POWERLEVEL9K_DIRENV_VISUAL_IDENTIFIER_EXPANSION=' '
 
   ###############[ asdf: asdf version manager (https://github.com/asdf-vm/asdf) ]###############
   # Default asdf color. Only used to display tools for which there is no color override (see below).
