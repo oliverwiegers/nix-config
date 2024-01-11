7c9493ef9c2b0c8d52d9ef0647b2fe046b814c9f
oliverwiegers <oliver.wiegers@gmail.com>
2024-01-11 07:27:17 +0100

    Update flake and restore unstable overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,15 @@
   outputs,
   inputs,
 }: {
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.unstable'
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      inherit (final) system;
+      config.allowUnfree = true;
+    };
+  };
+
   # For every flake input, aliases 'pkgs.inputs.${flake}' to
   # 'inputs.${flake}.packages.${pkgs.system}' or
   # 'inputs.${flake}.legacyPackages.${pkgs.system}'
