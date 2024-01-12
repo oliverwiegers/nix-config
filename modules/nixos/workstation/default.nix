dea5a1443993c0b5361570439fb604d7e5598d5f
oliverwiegers <oliver.wiegers@gmail.com>
2024-01-12 08:35:23 +0100

    Dynamically import configs in workstation module

--- modules/nixos/workstation/default.nix
+++ modules/nixos/workstation/default.nix
@@ -7,14 +7,14 @@
 with lib; let
   cfg = config.workstation;
 in {
-  imports = [
-    ./audio.nix
-    ./bluetooth.nix
-    ./laptop.nix
-    ./virtualization.nix
-    ./wifi.nix
-    ./wm.nix
-  ];
+  # Read all .nix file from local directory except "default.nix"
+  imports = map (path: ./. + "/${path}") (
+    builtins.filter (
+      file: lib.hasSuffix ".nix" file && file != "default.nix"
+    ) (
+      builtins.attrNames (builtins.readDir (builtins.toString ./.))
+    )
+  );
 
   options = {
     workstation = {
